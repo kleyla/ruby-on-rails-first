dd8e6963df0d9579e3b3c48873ec9abb17c15eba
kleyla <kleyla01@live.com>
2020-04-13 16:07:23 -0400

    Delete and before_action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,7 @@
 class ImagesController < ApplicationController
 
+    before_action :set_image, only: [:show, :edit, :update, :destroy]
+
     def index
         @images = Image.all 
     end
@@ -16,21 +18,32 @@ class ImagesController < ApplicationController
     end
     
     def show
-        @image = Image.find params[:id]
+        # @image = Image.find params[:id]
     end
     
     def edit
-        @image = Image.find params[:id]
+        # @image = Image.find params[:id]
     end
     
     def update
-        @image = Image.find params[:id]
+        # @image = Image.find params[:id]
         @image.update image_params
         redirect_to @image
+    end
+
+    def destroy
+        # @image = Image.find params[:id]
+        @image.destroy
+        redirect_to images_path
     end
 
     private
     def image_params
         params.require(:image).permit(:description)
     end
+
+    def set_image
+        @image = Image.find params[:id]
+    end
+
 end
